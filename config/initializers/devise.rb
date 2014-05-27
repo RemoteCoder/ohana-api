3c7a8880b8de294d85ac156ee0c0222ab5f628c8
RemoteCoder <shah.duet@gmail.com>
2014-05-28 01:36:03 +0600

    Devise secret key

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -247,6 +247,7 @@ Devise.setup do |config|
   # The secret key used by Devise. Devise uses this key to generate
   # random tokens. Changing this key will render invalid all existing
   # confirmation, reset password and unlock tokens in the database.
+  Rails.logger.info "==#{ENV['DEVISE_SECRET_KEY'].inspect}"
   if Rails.env.production? && ENV['DEVISE_SECRET_KEY'].blank?
     fail 'The DEVISE_SECRET_KEY environment variable is not set on your production' \
     ' server. To generate a random key, run "rake secret" from the command' \
